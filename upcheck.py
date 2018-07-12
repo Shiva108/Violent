d76a4737187be8631a9213104fda548b8157303f
e <esmurf@gmail.com>
2018-07-12 16:38:44 -0400

    Fixed upcheck.py

--- upcheck.py
+++ upcheck.py
@@ -6,4 +6,4 @@ nm = nmap.PortScanner()  # Constructing object nm
 nm.scan(hosts=target, arguments='-sn -Pn -PE')
 hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 for x, status in hosts_list:
-    print('{0}:{1}'.x)
+    print(x + " is " + status)
