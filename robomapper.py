2fb6d01eef593e54a0656bf4623a350d3c289070
e <esmurf@gmail.com>
2018-07-16 03:39:56 -0400

    version 0.0.2a scanning is working

--- robomapper.py
+++ robomapper.py
@@ -5,16 +5,17 @@
 # Script begins
 # ===============================================================================
 
-version = '0.0.1a'
+version = '0.0.2a'
 
 # Control Vars - Change these to change function of script
 openports = ' 22,80,8080'
 target = '10.10.10.83'
-targetname = "test_run"
+# targetname = "test_run" # For debugging purposes only
 pingscan = ' -sn -Pn'
 tcpscan = ' -sS -T4 -Pn -open -p- -vv'
 servicescan = ' -sSV -T4 -Pn -vv -p' + openports + ' ' + target
-outputdir = "/home/e/"
+outputdir = "~/"
+tunning = ' -T4 '  # Todo // for future version
 
 try:
     from colorama import init
@@ -40,13 +41,23 @@ def banner(version):
 
 def mapper(target, targetname, arguments):  # test target with parameters
     try:
-        print('Running nmap on ' + target + ' with arguments: ' + arguments)
+        print('[+] Running nmap on ' + target + ' with arguments: ' + arguments)
         nmscan = nmap.PortScanner()  # Constructing object
         nmscan.scan(hosts=target, arguments=arguments)
     except BaseException as e:
         print('Mapper has crashed: ' + e)
-    print('Mapping ' + target + ' with ' + arguments + ' is done without errors \n')
-    # print(nmscan.scaninfo())
+    # print('Mapping ' + target + ' with ' + arguments + ' is done without errors \n')
+    for host in nmscan.all_hosts():
+        # print('----------------------------------------------------')
+        # print('Host : %s (%s)' % (host, nmscan[host].hostname()))
+        # print('State : %s' % nmscan[host].state())
+        for proto in nmscan[host].all_protocols():
+            print('--------------------------------')
+            print('[+] Protocol : %s' % proto)
+            lport = list(nmscan[host][proto].keys())
+            lport.sort()
+            for port in lport:
+                print('[+] port : %s\tstate : %s' % (port, nmscan[host][proto][port]['state']))
 
 
 
@@ -54,7 +65,7 @@ def upcheck(target):
     nm = nmap.PortScanner()  # Constructing object nm
     nm.scan(hosts=target, arguments='-sn -Pn -PE')
     hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-    for x, status in hosts_list:
+    for x, status in hosts_list:  # For a possible multi target in later vs.
         print("[+] " + x + " is " + status)
 
 
@@ -101,7 +112,8 @@ def main():
         # Calls
         upcheck(target)
         # arguments = pingscan
-        mapper(target, targetname, pingscan)
+        # mapper(target, targetname, pingscan)
+        mapper(target, targetname, ' -sV -p 80,22 -Pn -vv -T4')
     except RuntimeError as e:
         print('Runtime error: ' + str(e))
 
